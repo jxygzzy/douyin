689498387288b08c7ca02912c4affe84b89d4ae8
jxygzzy <jxygzzy@163.com>
2022-05-30 09:06:59 +0800

    fix(): 🔥 修改数据库字段
    修改数据库字段，视频不直接存url

--- sql/heart.sql
+++ sql/heart.sql
@@ -1,17 +1,17 @@
 /*
  Navicat Premium Data Transfer
 
- Source Server         : mysql
+ Source Server         : 47.98.120.35
  Source Server Type    : MySQL
- Source Server Version : 80022
- Source Host           : localhost:3306
- Source Schema         : douyin
+ Source Server Version : 50736
+ Source Host           : 47.98.120.35:3306
+ Source Schema         : heart
 
  Target Server Type    : MySQL
- Target Server Version : 80022
+ Target Server Version : 50736
  File Encoding         : 65001
 
- Date: 29/05/2022 21:22:58
+ Date: 30/05/2022 09:05:16
 */
 
 SET NAMES utf8mb4;
@@ -22,59 +22,79 @@ SET FOREIGN_KEY_CHECKS = 0;
 -- ----------------------------
 DROP TABLE IF EXISTS `t_comment`;
 CREATE TABLE `t_comment`  (
-  `id` int(0) NOT NULL,
-  `video_id` int(0) NOT NULL,
+  `id` int(11) NOT NULL,
+  `video_id` int(11) NOT NULL,
   `content` varchar(1000) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `create_date` datetime(0) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '评论表' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
+-- Records of t_comment
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for t_favorite
 -- ----------------------------
 DROP TABLE IF EXISTS `t_favorite`;
 CREATE TABLE `t_favorite`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `video_id` int(0) NOT NULL,
-  `user_id` int(0) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `video_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '点赞表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_favorite
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for t_relation
 -- ----------------------------
 DROP TABLE IF EXISTS `t_relation`;
 CREATE TABLE `t_relation`  (
-  `id` int(0) NOT NULL,
-  `user_id` int(0) NOT NULL,
-  `to_user_id` int(0) NOT NULL COMMENT '被关注者id',
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `to_user_id` int(11) NOT NULL COMMENT '被关注者id',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '关系表，关注与被关注' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_relation
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for t_user
 -- ----------------------------
 DROP TABLE IF EXISTS `t_user`;
 CREATE TABLE `t_user`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `password` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `name` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '用户表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_user
+-- ----------------------------
 
 -- ----------------------------
 -- Table structure for t_video
 -- ----------------------------
 DROP TABLE IF EXISTS `t_video`;
 CREATE TABLE `t_video`  (
-  `id` int(0) NOT NULL AUTO_INCREMENT,
-  `play_url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `cover_url` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `user_id` int(0) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `play_key` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '对应七牛云的key，用于获取下载链接',
+  `cover_key` varchar(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `user_id` int(11) NOT NULL,
   `title` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `create_date` datetime(0) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '视频表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '视频表' ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_video
+-- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
